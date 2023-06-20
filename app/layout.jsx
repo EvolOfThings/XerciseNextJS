de4cdb99de13218d8c13e98c11397148b5364ad5
rishad <rishad.yamnoor@gmail.com>
2023-06-20 19:23:38 +0530

    app component structure and home page

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/globals.css';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "PromptAI",
+    description: "Discover and Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+  <html lang="en">
+<body>
+    <div className='main'>
+        <div className='gradient'>
+        </div>
+<main className='app'>
+    <Nav />
+    {children}
+</main>
+    </div>
+</body>
+  </html>
+    )
+}
+
+export default RootLayout
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 //nxtjs metadata apis to manage metadat
 //static md and dynamic md
 
@@ -54,9 +56,19 @@ export const metadata = {
 // }
 
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+<h1 className="head_text text-center">
+Discover & Share
+<br className="max-md:hidden"/>
+<span className="orange_gradient text-center">AI-Powered Prompts</span>
+</h1>
+<p className="desc text-center">
+  PromptAI is an open-source AI prompting tool.
+</p>
+<Feed />
+    </section>
   )
 }
 
