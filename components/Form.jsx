f708324c7bebf817d71fd0af2b0faff0c278e7de
rishad <rishad.yamnoor@gmail.com>
2023-06-28 19:04:30 +0530

    Form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({type, post, setPost, submitting, handleSubmit}) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type}</span>Post
+      </h1>
+      <p className="desc text-left max-w-max-md"> 
+        {type} and share amazing prompts with the world, and let your imaginaiton run wild with any AI-powered platform
+      </p>
+
+      <form
+      onSubmit={handleSubmit}
+      className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+<label>
+  <span className="font-satoshi font-semibold text-base text-gray-50">Your AI prompt</span>
+</label>
+      </form>
+      </section>
   )
 }
 
