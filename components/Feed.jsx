bd03b2fa14634aa288f1b08cb5e3e91b668048e9
rishad <rishad.yamnoor@gmail.com>
2023-07-04 19:58:26 +0530

    feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+
+import {useEffect, useState} from 'react'
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return(
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+      <PromptCard
+        key={post._id}
+        post={post}
+        handleTagClick={handleTagClick}
+      />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
+const [searchText, setSearchText] = useState('');
+const [posts, setPosts] = useState([]);
+
+const handleSearchChange = (e) => {
+
+}
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt');
+    const data = await response.ison();
+
+    setPosts(data);
+  }
+
+  fetchPosts();
+}, [])
+
+
+return (
+    <section className='feed'>
+      <Form className="relative w-full flex-center">
+        <input 
+        type="text"
+        value={searchText}
+        placeholder='Search for a tag or a username'
+        required
+        onChange={handleSearchChange} />
+      </Form>
+      
+    <PromptCardList
+      data={posts}
+      handleTagClick={()=>{ }}
+    />
+    </section>
   )
 }
 
